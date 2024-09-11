4110c072759d7eb78d81eb71eb976ee28d346e8e
a-ilyin <a-ilyin@it-serv.ru>
2024-09-11 23:21:33 +0300

    Актуализация старого проекта под новые тесты

--- internal/control/apiUserDeleteURLs.go
+++ internal/control/apiUserDeleteURLs.go
@@ -2,12 +2,13 @@ package control
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"strconv"
 )
 
-/*APIUserDeleteURLsHandler принимает список идентификаторов сокращённых URL для удаления в формате: [ "a", "b", "c", "d", ...].
+/*
+APIUserDeleteURLsHandler принимает список идентификаторов сокращённых URL для удаления в формате: [ "a", "b", "c", "d", ...].
 В случае успешного приёма запроса хендлер должен возвращать HTTP-статус 202 Accepted.
 */
 func (h DecoratedHandler) APIUserDeleteURLsHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,7 +19,7 @@ func (h DecoratedHandler) APIUserDeleteURLsHandler(w http.ResponseWriter, r *htt
 		w.WriteHeader(http.StatusNoContent)
 		return
 	}
-	bodyBytes, err := ioutil.ReadAll(r.Body)
+	bodyBytes, err := io.ReadAll(r.Body)
 	if err != nil {
 		fmt.Println("Проблемы с получением данных для удаления", err)
 		w.WriteHeader(http.StatusNoContent)
